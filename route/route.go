08ffd3693a3f109c9e8b00566ad471cad5cff2ff
wangxin <22388207+lzylyd@users.noreply.github.com>
2019-02-14 20:43:35 +0800

    first commit

---
+++ route/route.go
@@ -0,0 +1,7 @@
+package route
+
+import "github.com/gin-gonic/gin"
+
+func Routerlist(router *gin.Engine) {
+       router.GET("/routergroup",)
+}
---
+++ webcontroller/controller.go
@@ -0,0 +1,11 @@
+package webcontroller
+
+import "github.com/gin-gonic/gin"
+
+func Posthandle(ctx *gin.Context){
+	ctx.String(200,"hello")
+}
+//使用路由文件加载的路由
+func Routerhandle(ctx *gin.Context){
+	ctx.String(200,"router test")
+}
